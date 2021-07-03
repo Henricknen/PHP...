e6a85953ac44a5acd0787060b2bb811bd46dddc9
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2021-07-02 22:04:02 -0300

    operadores_logicos_matematicos

---
+++ 3.php
@@ -0,0 +1,51 @@
+<?php                                                    //Operadores logicos e matematicos
+
+    $num1 = 16;
+    $num2 = 5;
+
+    //Aritmeticos (Matematicos)
+    $res = $num1 + $num2;
+    echo "Soma de $num1 com $num2 = $res<br/>";
+    $res = $num1 - $num2;
+    echo "A subtração de $num1 com $num2 = $res <br/>;";
+    $res = $num1 / $num2;
+    echo "A divisão de $num1 por $num2 = $res<br/>";
+    $res = $num1 * $num2;
+    echo "A multiplicação de $num1 com $num2 = $res<br/>";
+
+    $res = $num1 % $num2;
+    echo "O resto da divisão de $num1 com $num2 = $res<hr/>";
+
+    $num1 = $num1 + 4;
+    echo "O novo valor de num1: $num1<br/>";
+
+    /* operadores de comparação
+    $num1 == $num2 - igual
+    $num1 != $num2 - diferente
+    $num1 > $num2 - maior que
+    $num1 < $num2 - menor que
+    $num1 >= $num2 - maior ou igual
+    $num1 <= $num2 - menor ou igual 
+
+    operadores logicos
+    AND - &&
+    OR - ||
+    */
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3 aula de PHP - Operações com variaveis</title>
+</head>
+<body>
+    
+</body>
+</html>
