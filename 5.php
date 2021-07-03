5f1648f9b563cb7f09abce714d6f3a7ea2026f12
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2021-07-02 22:04:54 -0300

    if_else

---
+++ 5.php
@@ -0,0 +1,30 @@
+<?php                                                               //IF-ELSE
+    $nota = 70;
+
+    if($nota >= 60)
+    {
+        echo "<font color=blue>Aprovado</font>";
+    }
+    else if ($nota >= 50)
+    {
+        echo "<font color=orange>Recuperação</font>";
+    }
+    else
+    {
+        echo "<font color_red>Reprovado</font>";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="ttext/html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>5 aula de PHP - Condicional IF-ELSE</title>
+</head>
+<body>
+
+</body>
+</html>
