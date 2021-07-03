0578246ef82e34b9e7ee762b0a9fa1789067b8bd
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2021-07-02 22:01:29 -0300

    html_php

---
+++ 1.php
@@ -0,0 +1,21 @@
+<?php                                                   // Codando em html/ php
+
+    echo "<font color=#ff0000>Primeira linha em PHP</font><br/>";
+    echo "Praticando PHP";
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1 aula de PHP</title>
+</head>
+<body>
+    <p> Codando em HTML</P>
+    <?php
+        echo "  Codando em PHP";
+    ?>
+</body>
+</html>
